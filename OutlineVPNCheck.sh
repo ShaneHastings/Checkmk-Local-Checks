b54c2b1832fab6b7a3cf4b7fa6f6dc69822d46fa
Shane Hastings <hastingsshane35@gmail.com>
2022-06-26 13:03:20 +0100

    Create OutlineVPNCheck.sh

---
+++ OutlineVPNCheck.sh
@@ -0,0 +1,10 @@
+# Check_mk Local check
+# Querys port 63280 to see if the system is listening on that port, as a proxy for
+# determining whether the Outline VPN server is running.
+# Author: Shane Hastings | 2022-06-26
+
+if [[ $(netstat -an | grep 63280 | grep -i listen) ]]; then
+    echo '0 "Outline VPN Service" - VPN service appears to be working. System is LISTENING on port>
+else
+    echo '2 "Outline VPN Service" - VPN service appears down. Shadowbox is NOT listening on port :>
+fi
