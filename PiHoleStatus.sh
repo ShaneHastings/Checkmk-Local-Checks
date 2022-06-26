598c64f43c6cf21f2f6b5373ddda71d506ec3450
Shane Hastings <hastingsshane35@gmail.com>
2022-06-26 15:47:23 +0100

    Update PiHoleStatus.sh

--- PiHoleStatus.sh
+++ PiHoleStatus.sh
@@ -4,7 +4,7 @@
 # Author: Shane Hastings | 2022-06-26
 
 if [[ $(pihole status | grep "enabled") ]]; then
-    echo '0 "PiHole DNS  Service" - Pi-Hole blocking is enabled on the network.'
+    echo '0 "PiHole DNS" - Pi-Hole blocking is enabled on the network.'
 else
-    echo '2 "PiHole DNS Service" - Pi-Hole is NOT enabled and DNS requests are not being processed.'
+    echo '2 "PiHole DNS" - Pi-Hole is NOT enabled and DNS requests are not being processed.'
 fi
