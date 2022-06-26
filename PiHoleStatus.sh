5388d1ce1e3746e3fd7e82ce3a8e5f945ff0f662
Shane Hastings <hastingsshane35@gmail.com>
2022-06-26 15:23:03 +0100

    Create PiHoleStatus.sh

---
+++ PiHoleStatus.sh
@@ -0,0 +1,10 @@
+# Check_mk Local check
+# Querys the PiHole service via "pihole status" and greps for enabled 
+# to determine whether the service is enabled and processing DNS queries.
+# Author: Shane Hastings | 2022-06-26
+
+if [[ $(pihole status | grep "enabled") ]]; then
+    echo '0 "PiHole DNS  Service" - Pi-Hole blocking is enabled on the network.'
+else
+    echo '2 "PiHole DNS Service" - Pi-Hole is NOT enabled and DNS requests are not being processed.'
+fi
